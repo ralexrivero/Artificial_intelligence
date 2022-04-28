7327fe370f6d37ce09e3e529e59807a052e75b4b
ralexrivero <ralexrivero@gmail.com>
2022-04-28 20:41:17 -0300

    apply

---
+++ DataCamp/intermediatePython/0x08-add_column.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" apply() to add a new column in a eficient mode """
+import pandas as pd
+
+
+cars = pd.read_csv('cars.csv', index_col=0)
+
+
+cars['COUNTRY']= cars['country'].apply(str.upper)
+print(cars)
